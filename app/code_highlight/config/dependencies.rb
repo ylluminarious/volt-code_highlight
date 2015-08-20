d5fa13a84818d19a775249b1151027c2e87595e0
George D. Plymale II <georgedp@orbitalimpact.com>
2015-08-20 12:22:21 -0400

    use sunburst theme instead of default

--- app/code_highlight/config/dependencies.rb
+++ app/code_highlight/config/dependencies.rb
@@ -1,5 +1,5 @@
 # Component dependencies
 disable_auto_import
 
-css_file '/assets/code_highlight/assets/css/default.css'
+css_file '/assets/code_highlight/assets/css/sunburst.css'
 javascript_file '/assets/code_highlight/assets/js/highlight.pack.js'
